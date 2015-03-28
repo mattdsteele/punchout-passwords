fb1f18c3222e554002b330e37e251de535e091ca
Matt Steele <orphum@gmail.com>
2015-03-28 13:59:43 -0500

    We don't need no stinking classes

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
 var _ = require('lodash');
 
-export default class PunchOut {
-  makeOffset(...args) {
-    return 255 - args.reduce((a,b) =>  a + b, 0);
-  }
-
-  addOffset(input) {
-    let withOffset = [];
-    let offsets = [6, 3, 5, 7, 9, 0, 1, 4, 2, 8];
-    input.forEach((val, i) => {
-      withOffset.push((val + offsets[i]) % 10);
-    });
-    return withOffset;
-  }
-
-  digits(val, bitPair) {
-    return _.padLeft(val.toString(2), 8, '0')
-      .split('')
-      .map(i => parseInt(i))
-      .slice(bitPair * 2, bitPair * 2 + 2);
-  }
-
-  intermediateBits(winHigh, winLow, lossHigh, lossLow, koHigh, koLow) {
-    return [0, 0]
-      .concat(this.digits(winHigh, 2))
-      .concat(this.digits(koLow, 3))
-      .concat([0, 0])
-      .concat(this.digits(koLow, 2))
-      .concat(this.digits(winHigh, 3))
-      .concat([0, 0])
-      .concat(this.digits(koHigh, 2))
-      .concat(this.digits(winLow, 3))
-      .concat([0, 0])
-      .concat(this.digits(winLow, 2))
-      .concat(this.digits(koHigh, 3))
-      .concat(this.digits(lossLow, 3))
-      .concat([0, 0]);
-  }
-
-  addOpponent(intermediate, checksum, opponent) {
-    intermediate.splice(0, 2, this.digits(checksum, 1)[0], this.digits(checksum, 1)[1]);
-    intermediate.splice(6, 2, this.digits(checksum, 2)[0], this.digits(checksum, 2)[1]);
-    intermediate.splice(12, 2, this.digits(checksum, 3)[0], this.digits(checksum, 3)[1]);
-    intermediate.splice(18, 2, this.digits(checksum, 0)[0], this.digits(checksum, 0)[1]);
-    intermediate.splice(26, 2, this.digits(opponent, 3)[0], this.digits(opponent, 3)[1]);
-    return intermediate;
-  }
+export function makeOffset(...args) {
+  return 255 - args.reduce((a,b) =>  a + b, 0);
 }
+
+export function addOffset(input) {
+  let withOffset = [];
+  let offsets = [6, 3, 5, 7, 9, 0, 1, 4, 2, 8];
+  input.forEach((val, i) => {
+    withOffset.push((val + offsets[i]) % 10);
+  });
+  return withOffset;
+}
+
+export function digits(val, bitPair) {
+  return _.padLeft(val.toString(2), 8, '0')
+    .split('')
+    .map(i => parseInt(i))
+    .slice(bitPair * 2, bitPair * 2 + 2);
+}
+
+export function intermediateBits(winHigh, winLow, lossHigh, lossLow, koHigh, koLow) {
+  return [0, 0]
+    .concat(digits(winHigh, 2))
+    .concat(digits(koLow, 3))
+    .concat([0, 0])
+    .concat(digits(koLow, 2))
+    .concat(digits(winHigh, 3))
+    .concat([0, 0])
+    .concat(digits(koHigh, 2))
+    .concat(digits(winLow, 3))
+    .concat([0, 0])
+    .concat(digits(winLow, 2))
+    .concat(digits(koHigh, 3))
+    .concat(digits(lossLow, 3))
+    .concat([0, 0]);
+}
+
+export function addOpponent(intermediate, checksum, opponent) {
+  intermediate.splice(0, 2, digits(checksum, 1)[0], digits(checksum, 1)[1]);
+  intermediate.splice(6, 2, digits(checksum, 2)[0], digits(checksum, 2)[1]);
+  intermediate.splice(12, 2, digits(checksum, 3)[0], digits(checksum, 3)[1]);
+  intermediate.splice(18, 2, digits(checksum, 0)[0], digits(checksum, 0)[1]);
+  intermediate.splice(26, 2, digits(opponent, 3)[0], digits(opponent, 3)[1]);
+  return intermediate;
+}
+
--- test.js
+++ test.js
@@ -1,26 +1,24 @@
-import PunchOut from './index.js';
+import {addOffset, makeOffset, intermediateBits, addOpponent} from './index.js';
 
 describe('punch out passwords', () => {
-  let punchOut;
-  beforeEach(() => { punchOut = new PunchOut() });
   it('adds an offset', () => {
     let input = [0, 3, 6, 5, 0, 1, 5, 2, 6, 5];
     let output = [6, 6, 1, 2, 9, 1, 6, 6, 8, 3];
 
-    expect(punchOut.addOffset(input)).toEqual(output);
+    expect(addOffset(input)).toEqual(output);
   });
 
   it('creates a checksum', ()=> {
-    expect(punchOut.makeOffset(2, 4, 0, 1, 1, 9)).toEqual(238);
+    expect(makeOffset(2, 4, 0, 1, 1, 9)).toEqual(238);
   });
 
   it('creates intermediate bits', ()=> {
     let expected = [0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0];
-    expect(punchOut.intermediateBits(2, 4, 0, 1, 1, 9)).toEqual(expected);
+    expect(intermediateBits(2, 4, 0, 1, 1, 9)).toEqual(expected);
   });
   it('creates intermediate bits', ()=> {
     let intermediate = [0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0];
     let expected =     [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0];
-    expect(punchOut.addOpponent(intermediate, 238, 2)).toEqual(expected);
+    expect(addOpponent(intermediate, 238, 2)).toEqual(expected);
   });
 });
